0623ed0c8068b6eb32174a6af6b07509148ac119
Michael Tanner <git@pixelwolf.ch>
2012-11-21 22:28:21 +0100

    improved geoblock (dport 22)

--- geoblock.sh
+++ geoblock.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for ip in `curl -s http://www.okean.com/sinokoreacidr.txt | grep -v "#" | awk '{print $1}'` ; do iptables -I INPUT -s $ip -j DROP ; done
+for ip in `curl -s http://www.okean.com/sinokoreacidr.txt | grep -v "#" | awk '{print $1}'` ; do iptables -I INPUT -s $ip -p tcp --dport 22 -j DROP ; done
