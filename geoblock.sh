ea48efbe0b9b4e149915136a669b8bd4063489bd
Michael Tanner <git@pixelwolf.ch>
2012-11-26 15:39:06 +0100

    added ability for multiple custom ranges

--- geoblock.sh
+++ geoblock.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-for ip in `curl -s http://www.okean.com/sinokoreacidr.txt | grep -v "#" | awk '{print $1}'` ; do iptables -I INPUT -s $ip -p tcp --dport 22 -j DROP ; done
+
+# custom ip ranges
+ranges="60.191.88.128/27"
+
+# curling file
+for ip in `curl -s http://www.okean.com/sinokoreacidr.txt | grep -v "#" | awk '{print $1}'` $ranges ; do iptables -I INPUT -s $ip -p tcp --dport 22 -j DROP ; done
