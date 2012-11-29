a9d5b20426d03c27818c5c2011e4bc51fd134b60
Michael Tanner <git@pixelwolf.ch>
2012-11-29 23:17:15 +0100

    added ability to provide ranges in another file

--- geoblock.sh
+++ geoblock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # custom ip ranges
-ranges="60.191.88.128/27"
+ranges=$(cat ranges)
 
 # curling file
 for ip in `curl -s http://www.okean.com/sinokoreacidr.txt | grep -v "#" | awk '{print $1}'` $ranges ; do iptables -I INPUT -s $ip -p tcp --dport 22 -j DROP ; done
