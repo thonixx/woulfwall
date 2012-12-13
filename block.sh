2b65483687b2a288d74ee8d8cd977de20defc455
Michael Tanner <git@pixelwolf.ch>
2012-12-13 21:02:19 +0100

    improved block and geoblock feature

--- block.sh
+++ block.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo $(echo $@ | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}') >> iptables.ranges
+if [ "$(echo $@ | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')" ]
+then
+	ip=$(echo $@ | egrep -o '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}')
+	echo "$ip" >> iptables.ranges
+	iptables -I INPUT -s $ip -p tcp --dport 22 -j DROP
+	iptables -I INPUT -s $ip -p tcp --dport 25 -j DROP
+	iptables -I INPUT -s $ip -p tcp --dport 993 -j DROP
+	iptables -I INPUT -s $ip -p tcp --dport 995 -j DROP
+	echo "added to block list"
+	exit 0
+else
+	echo "not an ip?"
+	exit 1
+fi
--- geoblock.sh
+++ geoblock.sh
@@ -8,4 +8,7 @@ for ip in `curl -s http://www.okean.com/sinokoreacidr.txt | grep -v "#" | awk '{
 do 
 	echo "Blocking $ip"
 	iptables -I INPUT -s $ip -p tcp --dport 22 -j DROP
+	iptables -I INPUT -s $ip -p tcp --dport 25 -j DROP
+	iptables -I INPUT -s $ip -p tcp --dport 993 -j DROP
+	iptables -I INPUT -s $ip -p tcp --dport 995 -j DROP
 done
